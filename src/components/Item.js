e5422756614b237952a37be4f7134545e8b30202
Jim Grimes <jimgrimes86@gmail.com>
2023-10-02 21:41:46 -0400

    DONE Dark Mode Toggle and Add to Cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, cartChange] = useState(false);
+
+  const cartClass = () => {
+    if (inCart === true) {
+      return "in-cart";
+    } else {
+      return "";
+    };
+  }
+
+  const buttonText = () => {
+    if(inCart === true) {
+      return "Remove From Cart"
+    } else {
+      return "Add to Cart"
+    }
+  }
+
+  function handleClick() {
+    cartChange((inCart) => inCart = !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={cartClass()}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonText()}</button>
     </li>
   );
 }
